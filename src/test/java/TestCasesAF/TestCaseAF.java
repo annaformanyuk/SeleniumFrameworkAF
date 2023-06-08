853d480313e94bd0dd8a7c91df2a8a01179495ae
annaformanyuk <anna.formanjuk@gmail.com>
2023-06-08 16:27:06 +0200

    first test commit

---
+++ src/test/java/TestCasesAF/TestCaseAF.java
@@ -0,0 +1,19 @@
+package TestCasesAF;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class TestCaseAF {
+
+    @Test
+    public void startBrowser(){
+        System.setProperty("webdriver.chrome.driver", "/Users/aformanjuk/IdeaProjects/SeleniumProjectAnnaF/src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.nytimes.com/");
+    }
+}
