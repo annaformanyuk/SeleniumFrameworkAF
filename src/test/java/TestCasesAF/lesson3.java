62d0c318c54ba5e9eba78be2b84cb858a593472b
annaformanyuk <anna.formanjuk@gmail.com>
2023-07-14 00:05:56 +0200

    update to lesson3

--- src/test/java/TestCasesAF/lesson3.java
+++ src/test/java/TestCasesAF/lesson3.java
@@ -1,5 +1,8 @@
 package TestCasesAF;
 
+import org.testng.annotations.Test;
+
+@Test
 public class lesson3 {
     public static void main(String[] args) {
 
@@ -9,22 +12,5 @@ public class lesson3 {
         int age = 5;
         String productName = "cheese";
         System.out.println("Hello "+ username +"! We have a special offer in new opened store in "+ location +". You can buy "+ productName +" with age older 5 years using price "+ price +"$.");
-    }
-}
-
-class lesson3_2 {
-    public static void main(String[] args) {
-
-        int firstmonth = 13;
-        int secondmonth = 20;
-        int thirdmonth = 9;
-        int total = firstmonth + secondmonth + thirdmonth;
-        int fee = 10;
-        int quantity = 3;
-
-        System.out.println(firstmonth + secondmonth + thirdmonth);
-        System.out.println((firstmonth + secondmonth + thirdmonth)/3);
-        System.out.println(total - fee);
-        System.out.println((firstmonth + secondmonth + thirdmonth) - (quantity * fee));
     }
 }
